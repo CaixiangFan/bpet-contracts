75d72a2cabc4379cb6f75d2ee4f1b0b5a11c55f5
caixiangfan <caixiang@ualberta.ca>
2022-07-28 18:21:46 +0000

    dev payment contract

--- contracts/Payment.sol
+++ contracts/Payment.sol
@@ -1,6 +1,86 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-contract Payment {
-  
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IPoolMarket {
+  struct DispatchedOffer {
+    address supplierAccount;
+    uint16 dispatchedAmount;
+  }
+
+  function getPoolPrice(uint hour) external view returns(uint16);
+  function getDispatchedOffers(uint hour) external view returns(DispatchedOffer[] memory);
+}
+
+interface IEnergyToken is IERC20 {
+  function transfer(address, uint256) external returns(bool);
+  function transferFrom(address, address, uint256) external returns (bool);
+  function approve(address, uint256) external returns (bool);
+}
+
+interface IRegistry {
+  function isRegisteredSupplier() external view returns(bool);
+  function isRegisteredConsumer() external view returns(bool);
+}
+
+contract Payment is Ownable {
+  address private marketAccount;
+  IEnergyToken public energyToken;
+  IPoolMarket public poolMarketContract;
+  IRegistry public registryContract;
+
+  modifier registeredSupplier() {
+    require(registryContract.isRegisteredSupplier(), "Unregistered supplier");
+    _;
+  }
+
+  modifier registeredConsumer() {
+    require(registryContract.isRegisteredConsumer(), "Unregistered consumer");
+    _;
+  }
+  constructor (
+    address _poolMarketAddress,
+    address _etkContractAddress,
+    address _registryContractAddress
+  ) {
+    marketAccount = msg.sender;
+    poolMarketContract = IPoolMarket(_poolMarketAddress);
+    energyToken = IEnergyToken(_etkContractAddress);
+    registryContract = IRegistry(_registryContractAddress);
+  }
+
+  /**
+  @dev Enable suppliers to charge from network/market only after service is done, namely the latest hour is the last hour.
+   */
+  function enableCharge(uint _hour) public onlyOwner{
+    //loop to approve all supplieres their corrresponding energy supply price
+    uint poolPrice = poolMarketContract.getPoolPrice(_hour);
+    uint len = poolMarketContract.getDispatchedOffers(_hour).length;
+    
+    for (uint i = 0; i < len; i++) {
+      address account = poolMarketContract.getDispatchedOffers(_hour)[i].supplierAccount;
+      uint amount = poolMarketContract.getDispatchedOffers(_hour)[i].dispatchedAmount;
+      energyToken.approve(account, amount * poolPrice);
+    }
+  }
+
+  /**
+  @dev Charges from the distribution network for a specific hour. The latest hour is the last hour.
+   */
+  function charge(uint _hour, uint _meteredAmount) public registeredSupplier{
+    uint poolPrice = poolMarketContract.getPoolPrice(_hour);
+    // should transfer from smart contract to generator
+    energyToken.transferFrom(marketAccount, msg.sender, poolPrice * _meteredAmount);
+  }
+
+  /**
+  @dev Pay EKT to the distribution network for a specific hour. The latest hour is the last hour.
+   */
+  function pay(uint _hour, uint _meteredAmount) public registeredConsumer {
+    uint poolPrice = poolMarketContract.getPoolPrice(_hour);
+    // should transfer from buyer to smart contract
+    energyToken.transfer(marketAccount, poolPrice * _meteredAmount);
+  }
 }
