86c172b49a12ce898d49a0f7f96ba05e1c15230d
caixiangfan <caixiang@ualberta.ca>
2022-08-24 22:38:06 +0000

    added burnable to energy token

--- contracts/EnergyToken.sol
+++ contracts/EnergyToken.sol
@@ -2,14 +2,25 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract EnergyToken is ERC20, AccessControl {
+
+  /**
+  @dev EnergyToken is designed to be a ERC20 stablecoin similar to USDC.
+  To buy energy tokens, buyers simply transfers fiat money (e.g., USD) to the market bank account.
+  The minter will mint the same amount of energy tokens to buyer's token account.
+  To redeem tokens, user submit a request with an amount. Market bank carefully verifies its identity.
+  Once verified, it transfers the amount of fiat money to user's bank account and burn the same amount of user's tokens.
+   */
+contract EnergyToken is ERC20, ERC20Burnable, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    address public owner;
 
     constructor() ERC20("EnergyToken", "ETK") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
+        owner = msg.sender;
     }
 
     function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
--- tests/energyToken.ts
+++ tests/energyToken.ts
@@ -41,35 +41,72 @@ describe("Testing ERC20 Token", () => {
       );
       expect(hasRole).to.eq(true);
     });
+  });
+  describe("when the minter mints tokens", async () => {
+    beforeEach(async () => {
+      const mintTx = await tokenContract.mint(
+        accounts[1].address,
+        TEST_MINT_VALUE
+      );
+      await mintTx.wait();
+    });
 
-    describe("when the minter call the mint function", async () => {
-      beforeEach(async () => {
-        const mintTx = await tokenContract.mint(
-          accounts[1].address,
-          TEST_MINT_VALUE
-        );
-        await mintTx.wait();
-      });
+    it("updates the total supply", async () => {
+      const totalSupplyBN = await tokenContract.totalSupply();
+      const expectedValueBN = TEST_MINT_VALUE;
+      const diffBN = totalSupplyBN.gt(expectedValueBN)
+        ? totalSupplyBN.sub(expectedValueBN)
+        : expectedValueBN.sub(totalSupplyBN);
+      const diff = Number(diffBN);
+      expect(diff).to.eq(0);
+    });
 
-      it("updates the total supply", async () => {
-        const totalSupplyBN = await tokenContract.totalSupply();
-        const expectedValueBN = TEST_MINT_VALUE;
-        const diffBN = totalSupplyBN.gt(expectedValueBN)
-          ? totalSupplyBN.sub(expectedValueBN)
-          : expectedValueBN.sub(totalSupplyBN);
-        const diff = Number(diffBN);
-        expect(diff).to.eq(0);
-      });
-
-      it("has given balance to the account", async () => {
-        const balanceOfBN = await tokenContract.balanceOf(accounts[1].address);
-        const expectedValueBN = TEST_MINT_VALUE;
-        const diffBN = balanceOfBN.gt(expectedValueBN)
-          ? balanceOfBN.sub(expectedValueBN)
-          : expectedValueBN.sub(balanceOfBN);
-        const diff = Number(diffBN);
-        expect(diff).to.eq(0);
-      });
+    it("has given balance to the account", async () => {
+      const balanceOfBN = await tokenContract.balanceOf(accounts[1].address);
+      const expectedValueBN = TEST_MINT_VALUE;
+      const diffBN = balanceOfBN.gt(expectedValueBN)
+        ? balanceOfBN.sub(expectedValueBN)
+        : expectedValueBN.sub(balanceOfBN);
+      const diff = Number(diffBN);
+      expect(diff).to.eq(0);
+    });
+  });
+  describe("when tokens are burned", async () => {
+    beforeEach(async () => {
+      const mintTx = await tokenContract.mint(
+        accounts[2].address,
+        TEST_MINT_VALUE
+      );
+      await mintTx.wait();
+    });
+    it("account has balance decreased", async () => {
+      const balanceOfBN = await tokenContract.balanceOf(accounts[2].address);
+      console.log('balance: ', ethers.utils.formatEther(balanceOfBN));
+      const burnAmountBN = ethers.utils.parseEther("5");
+      const owner = await tokenContract.owner();
+      const approveTx = await tokenContract.connect(accounts[2]).approve(owner, burnAmountBN);
+      await approveTx.wait();
+      const allowance = await tokenContract.allowance(accounts[2].address, owner);
+      console.log('Allowance: ', ethers.utils.formatEther(allowance));
+      expect(balanceOfBN).to.be.eq(TEST_MINT_VALUE);
+      const burnFromTx = await tokenContract.burnFrom(accounts[2].address, burnAmountBN);
+      await burnFromTx.wait();
+      const balanceNew = await tokenContract.balanceOf(accounts[2].address);
+      expect(balanceNew).to.eq(ethers.utils.parseEther("5"));
+    });
+    it("decreases the total token supply", async () => {
+      const totalSupplyBN = await tokenContract.totalSupply();
+      console.log('total supply: ', ethers.utils.formatEther(totalSupplyBN));
+      const owner = await tokenContract.owner();
+      const burnAmountBN = ethers.utils.parseEther("4");
+      const approveTx = await tokenContract.connect(accounts[2]).approve(owner, burnAmountBN);
+      await approveTx.wait();
+      expect(totalSupplyBN).to.be.eq(TEST_MINT_VALUE);
+      const burnFromTx = await tokenContract.burnFrom(accounts[2].address, burnAmountBN);
+      await burnFromTx.wait();
+      const newTotalSupplyBN = await tokenContract.totalSupply();
+      console.log('total supply afterwards: ', ethers.utils.formatEther(newTotalSupplyBN));
+      expect(newTotalSupplyBN).to.eq(ethers.utils.parseEther("6"));
     });
   });
 });
