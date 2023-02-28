20a6066543f7dc01d4d2f013681472a3a4f9455f
caixiangfan <caixiang@ualberta.ca>
2023-02-28 06:27:24 +0000

    update utils

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -1,14 +1,33 @@
-import { Bytes, ethers, BigNumber } from "ethers";
+import { Contract, ethers, BigNumber } from "ethers";
 import "dotenv/config";
+import * as registryJson from "../artifacts/contracts/Registry.sol/Registry.json";
 import * as tokenJson from "../artifacts/contracts/EnergyToken.sol/EnergyToken.json";
+import * as poolmarketJson from "../artifacts/contracts/PoolMarket.sol/PoolMarket.json";
 import { exit } from "process";
+import { PoolMarket, EnergyToken, Registry } from "../typechain";
 
 const EXPOSED_KEY =
   "8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f";
 const DEFAULT_RECEIVER = "0x8b7A99C5A9FD537370d94b68FBaf3F3FAfb50083";
 
 function setupProvider() {
-  const provider = ethers.providers.getDefaultProvider(process.env.BESU_URL);
+  var provider = ethers.providers.getDefaultProvider(process.env.BESU_URL);
+
+  if (process.env.PROVIDER_NETWORK == "GOERLI") {
+    const infuraOptions = process.env.INFURA_API_KEY
+      ? process.env.INFURA_API_SECRET
+        ? {
+            projectId: process.env.INFURA_API_KEY,
+            projectSecret: process.env.INFURA_API_SECRET,
+          }
+        : process.env.INFURA_API_KEY
+      : "";
+    const options = {
+      alchemy: process.env.ALCHEMY_API_KEY,
+      infura: infuraOptions,
+    };
+    provider = ethers.providers.getDefaultProvider("goerli", options);
+  }
   return provider;
 }
 
@@ -50,27 +69,49 @@ function convertBigNumberToNumber(value: BigNumber): number {
   return Math.round(Number(ethers.utils.formatEther(value)) * 10 ** decimals);
 }
 
-function setupGoerliProvider() {
-  const infuraOptions = process.env.INFURA_API_KEY
-    ? process.env.INFURA_API_SECRET
-      ? {
-          projectId: process.env.INFURA_API_KEY,
-          projectSecret: process.env.INFURA_API_SECRET,
-        }
-      : process.env.INFURA_API_KEY
-    : "";
-  const options = {
-    alchemy: process.env.ALCHEMY_API_KEY,
-    infura: infuraOptions,
-  };
-  const provider = ethers.providers.getDefaultProvider("goerli", options);
-  return provider;
+function getPoolMarketContract(wallet: ethers.Wallet): PoolMarket {
+  const provider = setupProvider();
+  const contractAddress = String(process.env.POOLMARKET_CONTRACT_ADDRESS);
+  const signer = wallet.connect(provider);
+  const contractInstance: PoolMarket = new Contract(
+    contractAddress,
+    poolmarketJson.abi,
+    signer
+  ) as PoolMarket;
+
+  return contractInstance;
+}
+
+function getETKContract(wallet: ethers.Wallet): EnergyToken {
+  const contractAddress: string = String(process.env.TOKEN_CONTRACT_ADDRESS);
+  const provider = setupProvider();
+  const signer = wallet.connect(provider);
+  const etkContractInstance: EnergyToken = new Contract(
+    contractAddress,
+    tokenJson.abi,
+    signer
+  ) as EnergyToken;
+  return etkContractInstance;
+}
+
+function getRegistryContract(wallet: ethers.Wallet) {
+  const registryContractAddress = String(process.env.REGISTRY_CONTRACT_ADDRESS);
+  const provider = setupProvider();
+  const registrySigner = wallet.connect(provider);
+  const registryContractInstance: Registry = new Contract(
+    registryContractAddress,
+    registryJson.abi,
+    registrySigner
+  ) as Registry;
+  return registryContractInstance;
 }
 
 export {
   EXPOSED_KEY,
   setupProvider,
-  setupGoerliProvider,
+  getPoolMarketContract,
+  getETKContract,
+  getRegistryContract,
   convertStringArrayToBytes32,
   attach,
   DEFAULT_RECEIVER,
